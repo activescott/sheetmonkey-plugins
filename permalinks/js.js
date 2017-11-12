d604b60f621fd1c6a879918d24c97983e737e240
Scott Willeke <scott@willeke.com>
2017-11-12 11:42:43 -0800

    fix for response api change

--- permalinks/js.js
+++ permalinks/js.js
@@ -18,9 +18,10 @@ function myCommandHandler (cmdInfo) {
         console.assert(container.containerType === 'sheet', 'unexpected container type:', container.containerType)
         const sheetID = container.containerID
         SheetMonkeyHost.apiRequest('GET', `sheets/${sheetID}?pageSize=1`, headers, data).then(response => {
-          console.log('api response:', response)
+          let sheetResponse = response.response
+          console.log('api response:', sheetResponse)
           // TODO: Consider this https://stackoverflow.com/a/30810322
-          window.prompt('Copy to clipboard: Ctrl+C, Enter', response.permalink)
+          window.prompt('Copy to clipboard: Ctrl+C, Enter', sheetResponse.permalink)
         })
       })
       break
